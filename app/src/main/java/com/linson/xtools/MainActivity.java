ee9e3e47a8deb1b0c396fbe0f2ca4e405a22737b
linson0116 <linson0116@163.com>
2017-03-27 12:14:40 +0800

    20170327

--- app/src/main/java/com/linson/xtools/MainActivity.java
+++ app/src/main/java/com/linson/xtools/MainActivity.java
@@ -1,14 +1,17 @@
 package com.linson.xtools;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.facebook.shimmer.ShimmerFrameLayout;
 import com.linson.xtools.app01.activity.CallActivity;
 import com.linson.xtools.app02.activity.Sms02Activity;
 import com.linson.xtools.app03.activity.Camera03Activity;
@@ -18,110 +21,100 @@ import com.linson.xtools.app09.activity.Test09Activity;
 import com.linson.xtools.utils.Lu;
 import com.linson.xtools.utils.PermissionUtils;
 
-public class MainActivity extends Activity implements View.OnClickListener{
-    //    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-//    private static String[] PERMISSIONS_STORAGE = {
-//            Manifest.permission.READ_EXTERNAL_STORAGE,
-//            Manifest.permission.WRITE_EXTERNAL_STORAGE};
-    private Button btn_1;
-    private Button btn_2;
-    private Button btn_3;
-    private Button btn_4;
-    private Button btn_5;
-    BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            btn_5.setText("已登录");
-        }
-    };
-    private Button btn_6;
-    private Button btn_7;
-    private Button btn_8;
-    private Button btn_9;
-
-//    public static void verifyStoragePermissions(Activity activity) {
-//        // Check if we have write permission
-//        int permission = ActivityCompat.checkSelfPermission(activity,
-//                Manifest.permission.WRITE_EXTERNAL_STORAGE);
-//        if (permission != PackageManager.PERMISSION_GRANTED) {
-//            // We don't have permission so prompt the user
-//            ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,
-//                    REQUEST_EXTERNAL_STORAGE);
-//        }
-//    }
+public class MainActivity extends Activity {
+    private static final String[] icon_names = {"短信电话", "短信记录", "拍照上传", "屏幕开关", "登录", "", "", "", "测试"};
+    private static final int[] icon = {R.drawable.icon_1, R.drawable.icon_2, R.drawable.icon_3, R.drawable.icon_4, R.drawable.icon_5, R.drawable.icon_6, R.drawable.icon_7, R.drawable.icon_8, R.drawable.icon_9};
+    private GridView gv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Lu.i("软件启动 onCreate");
-        init();
+        initUI();
     }
 
-    private void init() {
-        btn_1 = (Button) findViewById(R.id.btn_1);
-        btn_1.setOnClickListener(this);
-        btn_2 = (Button) findViewById(R.id.btn_2);
-        btn_2.setOnClickListener(this);
-        btn_3 = (Button) findViewById(R.id.btn_3);
-        btn_3.setOnClickListener(this);
-        btn_4 = (Button) findViewById(R.id.btn_4);
-        btn_4.setOnClickListener(this);
-        btn_5 = (Button) findViewById(R.id.btn_5);
-        btn_5.setOnClickListener(this);
-        btn_6 = (Button) findViewById(R.id.btn_6);
-        btn_6.setOnClickListener(this);
-        btn_7 = (Button) findViewById(R.id.btn_7);
-        btn_7.setOnClickListener(this);
-        btn_8 = (Button) findViewById(R.id.btn_8);
-        btn_8.setOnClickListener(this);
-        btn_9 = (Button) findViewById(R.id.btn_9);
-        btn_9.setOnClickListener(this);
+    private void initUI() {
+        //文字闪烁
+        ShimmerFrameLayout shimmer = (ShimmerFrameLayout) findViewById(R.id.shimmer_frame_layout);
+        shimmer.setDuration(3000);
+        shimmer.startShimmerAnimation();
+        //
+        gv = (GridView) findViewById(R.id.gv);
+        GridAdapter gridAdapter = new GridAdapter();
+        gv.setAdapter(gridAdapter);
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            Intent intent;
 
-        //verifyStoragePermissions(this);
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        Lu.i("点击按钮1 短信电话");
+                        intent = new Intent(getApplicationContext(), CallActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        Lu.i("点击按钮2 短信记录");
+                        intent = new Intent(getApplicationContext(), Sms02Activity.class);
+                        startActivity(intent);
+                        break;
+                    case 2:
+                        Lu.i("点击按钮3 拍照上传");
+                        intent = new Intent(getApplicationContext(), Camera03Activity.class);
+                        startActivity(intent);
+                        break;
+                    case 3:
+                        Lu.i("点击按钮4 屏幕解锁");
+                        intent = new Intent(getApplicationContext(), Screen04Activity.class);
+                        startActivity(intent);
+                        break;
+                    case 4:
+                        Lu.i("点击按钮5 登录");
+                        intent = new Intent(getApplicationContext(), Login05Activity.class);
+                        startActivity(intent);
+                        break;
+                    case 8:
+                        Lu.i("点击按钮9 测试");
+                        intent = new Intent(getApplicationContext(), Test09Activity.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
         PermissionUtils.verifyStoragePermissions(this);
-        IntentFilter intentFilter = new IntentFilter(Login05Activity.updateBtn_5);
-        registerReceiver(receiver, intentFilter);
     }
-    @Override
-    public void onClick(View v) {
-        Intent intent;
-        switch (v.getId()) {
-            case R.id.btn_1:
-                Lu.i("点击按钮1 短信电话");
-                intent = new Intent(getApplicationContext(), CallActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.btn_2:
-                Lu.i("点击按钮2 短信记录");
-                intent = new Intent(getApplicationContext(), Sms02Activity.class);
-                startActivity(intent);
-                break;
-            case R.id.btn_3:
-                Lu.i("点击按钮3 拍照上传");
-                intent = new Intent(getApplicationContext(), Camera03Activity.class);
-                startActivity(intent);
-                break;
-            case R.id.btn_4:
-                Lu.i("点击按钮4 屏幕解锁");
-                intent = new Intent(getApplicationContext(), Screen04Activity.class);
-                startActivity(intent);
-                break;
-            case R.id.btn_5:
-                Lu.i("点击按钮5 登录");
-                intent = new Intent(getApplicationContext(), Login05Activity.class);
-                startActivity(intent);
-                break;
-            case R.id.btn_9:
-                Lu.i("点击按钮9 测试");
-                intent = new Intent(getApplicationContext(), Test09Activity.class);
-                startActivity(intent);
-                break;
-        }
-    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(receiver);
+    }
+
+    class GridAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return icon_names.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = View.inflate(getApplicationContext(), R.layout.list_item_main_icon, null);
+            ImageView iv = (ImageView) view.findViewById(R.id.iv);
+            TextView tv = (TextView) view.findViewById(R.id.tv);
+            iv.setImageResource(icon[position]);
+            tv.setText(icon_names[position]);
+            return view;
+        }
     }
 }
