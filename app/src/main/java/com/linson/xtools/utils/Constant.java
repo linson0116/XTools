0269ce910a25f2410c3eb6501d58b32775d19e1a
linson <linson0116@163.com>
2017-12-24 14:17:34 +0800

    服务器ip 140.143.98.143

--- app/src/main/java/com/linson/xtools/utils/Constant.java
+++ app/src/main/java/com/linson/xtools/utils/Constant.java
@@ -4,19 +4,19 @@ package com.linson.xtools.utils;
  * Created by Administrator on 2016/5/23.
  */
 public class Constant {
-    public static final String USER_PATH = "http://123.56.111.226:8080/XToolsServer/UserServlet";
+    public static final String USER_PATH = "http://140.143.98.143:8080/XToolsServer/UserServlet";
 //    public static final String USER_PATH = "http://192.168.8.19:8080/XToolsServer/UserServlet";
 
     //public static final String LOG_PATH = "http://123.56.111.226:8080/XToolsServer/LogServlet";
     public static final String LOG_PATH = "http://192.168.8.19:8080/XToolsServer/LogServlet";
 
     //public static final String UPLOADFILE_PATH = "http://123.56.111.226:8080/XToolsServer/ServletUpFile";
-    public static final String UPLOADFILE_PATH_CLOUD = "http://123.56.111.226:8080/XToolsServer/ServletUpFile";
+    public static final String UPLOADFILE_PATH_CLOUD = "http://140.143.98.143:8080/XToolsServer/ServletUpFile";
     public static final String UPLOADFILE_PATH = "http://192.168.8.19:8080/XToolsServer/ServletUpFile";
     public static final String UPLOADFILE_PATH_LOCAL = "http://192.168.8.19:8080/XToolsServer/ServletUpFile";
 
     //public static final String IMAGE_INFO_PATH = "http://123.56.111.226:8080/XToolsServer/ImageInfoServlet";
-    public static final String IMAGE_INFO_PATH_CLOUD = "http://123.56.111.226:8080/XToolsServer/ImageInfoServlet";
+    public static final String IMAGE_INFO_PATH_CLOUD = "http://140.143.98.143:8080/XToolsServer/ImageInfoServlet";
     public static final String IMAGE_INFO_PATH = "http://192.168.8.19:8080/XToolsServer/ImageInfoServlet";
     public static final String IMAGE_INFO_PATH_LOCAL = "http://192.168.8.19:8080/XToolsServer/ImageInfoServlet";
 
